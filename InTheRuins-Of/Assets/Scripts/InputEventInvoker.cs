89ccaa710f1157b88a88a70bd9270cec8e573d0a
Samuel Förström <samppa.f@gmail.com>
2020-02-13 14:48:09 +0200

    Mathematics, VFXGraph, my MyUnityCollection

--- InTheRuins-Of/Assets/Scripts/InputEventInvoker.cs
+++ InTheRuins-Of/Assets/Scripts/InputEventInvoker.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerWeaponHandler : MonoBehaviour {
+public class InputEventInvoker : MonoBehaviour {
+  public
+
+  struct InputEvent {
+
+  }
+
   // Start is called before the first frame update
   void Start() {
 
@@ -10,11 +16,14 @@ public class PlayerWeaponHandler : MonoBehaviour {
 
   // Update is called once per frame
   void Update() {
-
+    if (Input.GetKeyDown(attack))
+      attackDown = true;
   }
 
   // Update is called once per frame
   void FixedUpdate() {
-
+    if (attackDown) {
+      attackDown = false;
+    }
   }
 }
