b1b2d7a74df2fda89f52f3a0911ce966d8b907d7
Samuel Förström <samppa.f@gmail.com>
2020-02-17 14:44:19 +0200

    OnTriggerEvent ja tein muutoksia pelaajan objekti koostumukseen ja komponenttiominaisuuksiin

---
+++ InTheRuins-Of/Assets/MyUnityCollection/Scripts/Physics/3D/OnTriggerEvent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class OnTriggerEvent : MonoBehaviour {
+  [Tooltip("Invoked when " + nameof(OnTriggerEnter) + " is triggered")]
+  public TriggerUnityEvent enterEvent;
+  [Tooltip("Invoked when " + nameof(OnTriggerStay) + " is triggered")]
+  public TriggerUnityEvent stayEvent;
+  [Tooltip("Invoked when " + nameof(OnTriggerExit) + " is triggered")]
+  public TriggerUnityEvent exitEvent;
+
+  [System.Serializable]
+  public class TriggerUnityEvent : UnityEvent<Collider> { }
+
+  // Start is called before the first frame update
+  void Start() {
+    foreach (var col in GetComponents<Collider>()) {
+      if (col.isTrigger) return;
+    }
+    throw new UnityException($"{nameof(OnTriggerEvent)} requires a trigger collider to exist on the GameObject");
+  }
+
+  public void Test(Collider col) {
+    print(col);
+  }
+
+  void OnTriggerEnter(Collider col) => enterEvent.Invoke(col);
+  void OnTriggerExit(Collider col) => exitEvent.Invoke(col);
+  void OnTriggerStay(Collider col) => stayEvent.Invoke(col);
+}
