57ca3df9afb9d4edf875d9f0dfa00debbe67ae1d
Samuel Förström <samppa.f@gmail.com>
2020-02-17 16:30:46 +0200

    Enenenenemy spotted

---
+++ InTheRuins-Of/Assets/Scripts/Enemy.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Untitled Enemy", menuName = "NPC/Enemy")]
+public class Enemy : ScriptableObject {
+  public new string name;
+  public float health;
+  public float armor;
+  public float damage;
+
+  public float speed;
+}
