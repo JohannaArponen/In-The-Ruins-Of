e230149cb5539b1530ea91a7fd418604516f25ca
Samuel Förström <samppa.f@gmail.com>
2020-02-26 17:35:30 +0200

    Update CircularBuffer.cs

--- InTheRuins-Of/Assets/MyUnityCollection/Scripts/Collections/ModifierList/CircularBuffer.cs
+++ InTheRuins-Of/Assets/MyUnityCollection/Scripts/Collections/ModifierList/CircularBuffer.cs
@@ -1,20 +1,43 @@
-﻿using System.Collections.Generic;
-using System;
+﻿using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
 
-public class CircularBuffer<T> {
+/// <summary>
+/// Represents a first-in, first-out fixed size list of objects. 
+/// /// </summary>
+public class CircularBuffer<T> : IEnumerable<T> {
+
+  public T this[int index] {
+    get => data[head - index];
+    set => data[head - index] = value;
+  }
+
+  public int Length { get => data.Length; }
 
   private T[] data;
-  private CircularInt _head;
-
-
-
-  protected Stack<T> list;
-
+  private CircularInt head;
 
   public CircularBuffer(int length) {
     data = new T[length];
+    head = new CircularInt(0, length);
+
+    var a = new List<int>();
+    var b = a.AsReadOnly();
   }
 
-  public void Pop() {
+  public void Push(T item) {
+    data[++head] = item;
   }
+
+  IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+  public IEnumerator<T> GetEnumerator() {
+    for (int i = 0; i < head.ceil; i++) {
+      yield return data[head - i];
+    }
+  }
+
+  public ReadOnlyCollection<T> AsReadOnly() => new ReadOnlyCollection<T>(this.ToArray());
+
+  public new string ToString() => string.Join(", ", this.ToArray());
 }
