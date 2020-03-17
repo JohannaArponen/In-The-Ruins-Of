6517ff40d4880ea5e2564b99af35b8b20668520d
Johanna Arponen <johanna.arponen@icloud.com>
2020-03-17 09:38:21 +0200

    Inventory

---
+++ InTheRuins-Of/Assets/openInventoryandClose.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openInventoryandClose : MonoBehaviour{
+
+    public static bool GameIsPause = false;
+    public GameObject pauseInventoryUI;
+    
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (GameIsPause) {
+                Resume();
+            } else {
+                OpenIn();
+            }
+        }
+    }
+
+    void Resume() {
+        pauseInventoryUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPause = false;
+    }
+    void OpenIn() {
+        pauseInventoryUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPause = true;
+    }
+
+    public void Test() {
+        print("testi");
+    }
+}
